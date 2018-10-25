d3db8a88a7462d76277b544c9aa2af22cde95f4b
Tiago Gimenes <tlgimenes@gmail.com>
2018-10-25 17:47:21 -0300

    apps without the +build sufix should find its spec as well

--- node/common/spec.ts
+++ node/common/spec.ts
@@ -1,5 +1,5 @@
-import { Apps, DiskCache, LRUCache, MultilayeredCache } from '@vtex/api'
-import { keys, prop } from 'ramda'
+import { Apps, DiskCache, LRUCache, MultilayeredCache, removeBuild } from '@vtex/api'
+import { find, keys, prop } from 'ramda'
 
 import { CACHE_PATH, SPEC_PATH, VEGA_GRAPHQL } from './globals'
 import { ignoreNotFound } from './notFound'
@@ -12,14 +12,23 @@ const specStorage = new MultilayeredCache<string, Maybe<Specs>>([
   new DiskCache(CACHE_PATH)
 ])
 
+const equalsNoBuild = (appIdNoBuild: string) => (appId: string) => removeBuild(appIdNoBuild) === removeBuild(appId)
+
+const findAppInDeps = (apps: Apps, appIdNoBuild: string): Promise<Maybe<string>> => 
+  apps.getDependencies()
+  .then(keys)
+  .then(find(equalsNoBuild(appIdNoBuild)))
+
 const fetchSpec = (apps: Apps, appId: string) => (): Promise<Maybe<Specs>> => 
   apps.getAppFile(appId, SPEC_PATH)
   .then(toString)
   .then(JSON.parse)
   .catch(ignoreNotFound(null))
 
-export const getSpecs = (apps: Apps, appId: string) => 
-  specStorage.get(cacheKey(appId), fetchSpec(apps, appId))
+export const getSpecs = async (apps: Apps, appId: string) => {
+  const appIdWithBuild = await findAppInDeps(apps, removeBuild(appId))
+  return appIdWithBuild && specStorage.get(cacheKey(appIdWithBuild), fetchSpec(apps, appIdWithBuild))
+}
 
 export const getSpec = (apps: Apps, appId: string, specName: string): Promise<string> => 
   getSpecs(apps, appId).then(prop(specName))
